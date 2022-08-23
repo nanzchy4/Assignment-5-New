11738d09d1837e9d2bcaff42acc1d9961b7c89b9
Nanzi Mustafa Chowdhury <ge42poj@mytum.de>
2022-08-23 23:09:02 +0600

    indentaion done

--- js/select.js
+++ js/select.js
@@ -1,4 +1,3 @@
-
 // event handler start 
 document.getElementById('select-1').addEventListener('click', function(event){
     
@@ -6,29 +5,22 @@ document.getElementById('select-1').addEventListener('click', function(event){
     if(buttonDisableCheck==0){
         return;
     }
-    disableButton(event);
-
+    disableButton(event);    
     
 })
-document.getElementById('select-2').addEventListener('click', function(event){
-    // const playerName = getPlayerName('player-2')
-    // updatePlayerList(playerName);
-    // disableButton(event);
-    // const buttonDisableCheck = lengthCheck(player-);
 
+document.getElementById('select-2').addEventListener('click', function(event){
+    
     const buttonDisableCheck = lengthCheck("player-2");
     if(buttonDisableCheck==0){
         return;
     }
     disableButton(event);
 
-    
 })
+
 document.getElementById('select-3').addEventListener('click', function(event){
-    // const playerName = getPlayerName('player-3')
-    // updatePlayerList(playerName);
-    // disableButton(event);
-    // lengthCheck(player-);
+    
     const buttonDisableCheck = lengthCheck("player-3");
     if(buttonDisableCheck==0){
         return;
@@ -36,11 +28,9 @@ document.getElementById('select-3').addEventListener('click', function(event){
     disableButton(event);
 
 })
+
 document.getElementById('select-4').addEventListener('click', function(event){
-    // const playerName = getPlayerName('player-4')
-    // updatePlayerList(playerName);
-    // disableButton(event);
-    // lengthCheck(player-);
+
     const buttonDisableCheck = lengthCheck("player-4");
     if(buttonDisableCheck==0){
         return;
@@ -48,11 +38,9 @@ document.getElementById('select-4').addEventListener('click', function(event){
     disableButton(event);
 
 })
+
 document.getElementById('select-5').addEventListener('click', function(event){
-    // const playerName = getPlayerName('player-5')
-    // updatePlayerList(playerName);
-    // disableButton(event);
-    // lengthCheck(player-);
+    
     const buttonDisableCheck = lengthCheck("player-5");
     if(buttonDisableCheck==0){
         return;
@@ -60,11 +48,9 @@ document.getElementById('select-5').addEventListener('click', function(event){
     disableButton(event);
 
 })
+
 document.getElementById('select-6').addEventListener('click', function(event){
-    // const playerName = getPlayerName('player-6')
-    // updatePlayerList(playerName);
-    // disableButton(event);
-    // lengthCheck(player-);
+   
     const buttonDisableCheck = lengthCheck("player-6");
     if(buttonDisableCheck==0){
         return;
